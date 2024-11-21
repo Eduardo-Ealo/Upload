6c01538262b21ef78a867b5e53b2067ea8a343ba
Eduardo Ealo <eduardoealo4515@gmail.com>
2024-11-21 09:49:17 -0500

    Upload para desplegar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000, // Usa el puerto que Render asigna dinámicamente
+  },
+});
