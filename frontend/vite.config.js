befbb7cb1b6fb0db86986fd7d300603685d51649
Eduardo Ealo <eduardoealo4515@gmail.com>
2024-11-21 11:19:00 -0500

    volviendo a estado original de vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  server: {
-    host: '0.0.0.0',
-    port: 5173, // or any other port you prefer
-  },
-};
-
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
